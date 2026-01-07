3a66c24d19b70ccabfd713515b39bb16366c492e
Shivam Seksaria <Shivamseksaria41@gmail.com>
2026-01-07 14:09:43 +0530

    Added padding to the code element so that code text doesn't stick to the walls of the code container

--- src/components/CodeElement.jsx
+++ src/components/CodeElement.jsx
@@ -9,7 +9,7 @@ export const CodeElement = ({ element, onUpdate }) => (
     <textarea
       value={element.content}
       onChange={(e) => onUpdate(element.id, e.target.value)}
-      className="w-full h-full bg-transparent text-black font-mono text-sm resize-none focus:outline-none"
+      className="p-2 w-full h-full bg-transparent text-black font-mono text-sm resize-none focus:outline-none"
       style={{
         caretColor: "black",
         overflow: "hidden",
